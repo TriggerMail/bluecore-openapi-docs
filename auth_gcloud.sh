3ce2d07f8808a48e493c1fe8b672d7633d7fd710
Michael Myung <michael.myung@bluecore.com>
2020-12-11 15:14:18 -0500

    adding gcloud list

--- auth_gcloud.sh
+++ auth_gcloud.sh
@@ -1,3 +1,5 @@
 echo $GCLOUD_ACCOUNT_CRED > gcloud.json
+echo "$GCLOUD_ACCOUNT_CRED"
 gcloud auth activate-service-account 270647425515-it0qf6jth5r14sot1oifartsm52cbe60@developer.gserviceaccount.com --key-file gcloud.json
+gcloud auth list
 gcloud config set project triggeredmail
