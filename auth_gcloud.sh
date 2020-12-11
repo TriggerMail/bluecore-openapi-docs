627079ef11bd29ad9d082f258a275dd7cbddcc5e
Michael Myung <michael.myung@bluecore.com>
2020-12-11 16:16:14 -0500

    adding relative filepath to gcloud.json

--- auth_gcloud.sh
+++ auth_gcloud.sh
@@ -1,4 +1,4 @@
-echo $GCLOUD_ACCOUNT_CRED > gcloud.json
+echo $GCLOUD_ACCOUNT_CRED > ./gcloud.json
 cat gcloud.json
 gcloud auth activate-service-account 270647425515-it0qf6jth5r14sot1oifartsm52cbe60@developer.gserviceaccount.com --key-file gcloud.json
 gcloud config set project triggeredmail
