717c818a082c773c71996cf78d7a2d9736aef824
Michael Myung <michael.myung@bluecore.com>
2020-12-11 15:46:49 -0500

    adding cat gcloud.json to read contents

--- auth_gcloud.sh
+++ auth_gcloud.sh
@@ -1,5 +1,4 @@
 echo $GCLOUD_ACCOUNT_CRED > gcloud.json
-echo "$GCLOUD_ACCOUNT_CRED"
+cat gcloud.json
 gcloud auth activate-service-account 270647425515-it0qf6jth5r14sot1oifartsm52cbe60@developer.gserviceaccount.com --key-file gcloud.json
-gcloud auth list
 gcloud config set project triggeredmail
