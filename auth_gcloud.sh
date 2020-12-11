604e0725dd616ff09765fbc22b404457117ca820
Michael Myung <michael.myung@bluecore.com>
2020-12-11 16:26:48 -0500

    adding chown

--- auth_gcloud.sh
+++ auth_gcloud.sh
@@ -1,4 +1,5 @@
-echo $GCLOUD_ACCOUNT_CRED > ./tmp/gcloud.json
+chown -R $USER:$USER ./
+echo $GCLOUD_ACCOUNT_CRED > gcloud.json
 cat gcloud.json
 gcloud auth activate-service-account 270647425515-it0qf6jth5r14sot1oifartsm52cbe60@developer.gserviceaccount.com --key-file gcloud.json
 gcloud config set project triggeredmail
